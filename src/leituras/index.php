b9322fd8ea36b6dba8fc94126cc7b32107a193cf
Protocol Corporation <protocollive@outlook.com>
2021-09-07 12:47:09 -0300

    Array com os nome dos tempos

--- src/leituras/index.php
+++ src/leituras/index.php
@@ -1,5 +1,5 @@
 <?php
-//2021.09.07.03
+//2021.09.07.04
 //Protocol Corporation Ltda.
 https://github.com/SantuarioMisericordiaRJ/StbModuleLeituras
 
@@ -26,22 +26,18 @@ function Command_leitura(){
   global $Bot, $Language;
   $AnoLiturgico = new AnoLiturgico();
   $Tempos = [
-    'tc' => 'Tempo comum',
-    'adv' => 'Advento',
-    'qrm' => 'Quaresma',
-    'ntl' => 'Natal'
+    AnoLiturgico::TempoComum => ['tc', 'Tempo comum'],
+    AnoLiturgico::TempoAdvento => ['adv', 'Advento'],
+    AnoLiturgico::TempoQuaresma => ['qrm', 'Quaresma'],
+    AnoLiturgico::TempoNatal => ['ntl', 'Natal']
   ];
   $index = file_get_contents('https://raw.githubusercontent.com/SantuarioMisericordiaRJ/ApiCatolica/main/src/index-min.json');
   $especiais = file_get_contents('https://raw.githubusercontent.com/SantuarioMisericordiaRJ/ApiCatolica/main/src/especiais-min.json');
   $index = json_decode($index, true);
   $especiais = json_decode($especiais, true);
-  $temp = $AnoLiturgico->TempoGet(time());
+  list($tempo, $semana) = $AnoLiturgico->TempoGet(time());
   $hoje = date('Y-m-d');
 
-  if(isset($temp[0]) and $temp[0] === AnoLiturgico::TempoComum):
-    $tempo = 'tc';
-    $semana = $temp[1];
-  endif;
   $DiaSemana = date('N');
   if($DiaSemana === '7'):
     $ano = AnoLetra();
@@ -50,13 +46,13 @@ function Command_leitura(){
   else:
     $ano = 'i';
   endif;
-  $l1 = $especiais[$hoje][1] ?? $index[$tempo][$semana][$DiaSemana][$ano][1];
-  $r = $especiais[$hoje]['r'] ?? $index[$tempo][$semana][$DiaSemana][$ano]['r'];
-  $l2 = $especiais[$hoje][2] ?? $index[$tempo][$semana][$DiaSemana][$ano][2] ?? null;
+  $l1 = $especiais[$hoje][1] ?? $index[$Tempos[$tempo][0]][$semana][$DiaSemana][$ano][1];
+  $r = $especiais[$hoje]['r'] ?? $index[$Tempos[$tempo][0]][$semana][$DiaSemana][$ano]['r'];
+  $l2 = $especiais[$hoje][2] ?? $index[$Tempos[$tempo][0]][$semana][$DiaSemana][$ano][2] ?? null;
   //Evangelho: especial ou domingo ou dia de semana...
-  $e = $especiais[$hoje]['e'] ?? $e = $index[$tempo][$semana][$DiaSemana][$ano]['e'] ?? $index[$tempo][$semana][$DiaSemana]['e'];
+  $e = $especiais[$hoje]['e'] ?? $e = $index[$Tempos[$tempo][0]][$semana][$DiaSemana][$ano]['e'] ?? $index[$Tempos[$tempo][0]][$semana][$DiaSemana]['e'];
 
-  $texto = '<b>' . $semana . 'ª semana do ' . $Tempos[$tempo] . ' - ' . $Language->TextGet('WeekDay' . $DiaSemana) . "</b>\n";
+  $texto = '<b>' . $semana . 'ª semana do ' . $Tempos[$tempo][1] . ' - ' . $Language->TextGet('WeekDay' . $DiaSemana) . "</b>\n";
   if(isset($especiais[$hoje]['nome'])):
     $texto .= '<b>' . $especiais[$hoje]['nome'] . "</b>\n";
   endif;
